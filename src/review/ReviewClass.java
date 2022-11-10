45a01bf924f98777d2efa66b69a244c99cbd9c12
pantherstek <117786371+pantherstek@users.noreply.github.com>
2022-11-09 20:21:21 -0500

    Update ReviewClass.java
    Main method and review method removed

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,13 +2,6 @@ package review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 	
-	public void test() {
-		System.out.println("This method is on Review Session Branch");
-	}
 }
